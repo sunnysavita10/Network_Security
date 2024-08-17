0a8d13533be71997a723861f0f96676169ed5720
sunnysavita10 <engsunny3365@gmail.com>
2024-08-17 12:20:23 +0530

    setp.py and requirement added

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+    
+    try:
+        # Open and read the requirements.txt file
+        with open('requirements.txt', 'r') as file:
+            # Read lines from the file
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                # Strip whitespace and newline characters
+                requirement = line.strip()
+                # Ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    
+        
+    return requirement_list
+print(get_requirements())
+setup(
+    name="networksecurity",
+    version="0.0.1",
+    author="sunny savita",
+    author_email="snshrivas3365@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements()
+)
