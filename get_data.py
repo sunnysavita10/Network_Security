90bc439a0275b59630149b16eb4a7828ccfe0d2a
sunnysavita10 <engsunny3365@gmail.com>
2024-08-17 12:34:41 +0530

    exception and logger and get data updated

--- get_data.py
+++ get_data.py
@@ -3,14 +3,17 @@ import sys
 import json
 
 from dotenv import load_dotenv
-
+load_dotenv()
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
 
 import certifi
+ca = certifi.where()
+
 import pandas as pd
 import numpy as np
 import pymongo
-
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
