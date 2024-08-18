98f231dfb21bd7ac6e9109268d48e7e1b56328d0
sunnysavita10 <engsunny3365@gmail.com>
2024-08-18 11:16:23 +0530

    config and artifact structure updated

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    pass
+
+@dataclass
+class DataValidationArtifact:
+    pass
+
+@dataclass
+class DataTransformationArtifact:
+    pass 
+
+@dataclass
+class ModelTrainerArtifact:
+    pass
+
+@dataclass
+class ModelEvaluationArtifact:
+    pass
+
+@dataclass
+class ModelPusherArtifact:
+    pass
+
+@dataclass
+class ClassificationMetricArtifact:
+    pass
---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import os
+from networksecurity.constant import training_pipeline
+
+print(training_pipeline.ARTIFACT_DIR)
+
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        pass
+    
+class DataIngestionConfig:
+    def __init__(self):
+        pass
+    
+class DataValidationConfig:
+    def __init__(self):
+        pass
+    
+
+class DataTransformationConfig:
+    def __init__(self):
+        pass
+    
+class ModelTrainerConfig:
+    def __init__(self):
+        pass
+    
+class ModelEvaluationConfig:
+    def __init__(self):
+        pass
+    
+class ModelPusherConfig:
+     def __init__(self):
+        pass
