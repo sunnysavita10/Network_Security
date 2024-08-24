5df3c958378897e2fc8098388ff9cea8cc063322
sunnysavita10 <engsunny3365@gmail.com>
2024-08-24 10:43:21 +0530

    data validation started

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -49,9 +49,12 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_validation(self):
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
-            pass
+            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            data_validation_artifact=data_validation.initiate_data_validation()
+            return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -83,6 +86,7 @@ class TrainingPipeline:
         try:
             data_ingestion_artifact=self.start_data_ingestion()
             print(data_ingestion_artifact)
+            data_validation=self.start_data_validation()
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
