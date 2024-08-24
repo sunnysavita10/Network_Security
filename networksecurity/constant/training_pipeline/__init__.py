a0fc19e16746809fb05e7b0ac9676d98de92fefe
sunnysavita10 <engsunny3365@gmail.com>
2024-08-24 10:51:59 +0530

    validation config and entity added

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -34,7 +34,11 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 """
 Data Validation related constant start with DATA_VALIDATION VAR NAME
 """
-
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 """
 Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,7 +7,13 @@ class DataIngestionArtifact:
 
 @dataclass
 class DataValidationArtifact:
-    pass
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
+    
 
 @dataclass
 class DataTransformationArtifact:
