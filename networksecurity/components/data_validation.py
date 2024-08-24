8d9ac678215a292b939c370169122c7960e09422
sunnysavita10 <engsunny3365@gmail.com>
2024-08-24 11:21:28 +0530

    utils file added

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -9,13 +9,21 @@ import pandas as pd
 import os,sys
 
 class DataValidation:
-    def __init__(self):
-        pass
+    def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
+                 data_validation_config:DataValidationConfig):
+        
+        try:
+            self.data_ingestion_artifact=data_ingestion_artifact
+            self.data_validation_config=data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
     
     
     def validate_number_of_column(self,dataframe:pd.DataFrame)->bool:
         try:
             pass
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,27 @@
+import yaml
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+import os,sys
+import numpy as np
+import dill
+import pickle
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
