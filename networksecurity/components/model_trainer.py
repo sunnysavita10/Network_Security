344a9b0084d8630af1309aeda6c052c592a0b927
sunnysavita10 <engsunny3365@gmail.com>
2024-08-24 13:02:19 +0530

    model training prepration is done

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,3 +1,21 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException 
+from networksecurity.logger.logger import logging
+
+from networksecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
+from xgboost import XGBClassifier
+
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.utils.main_utils.utils import save_object,load_object
+from networksecurity.utils.main_utils.utils import load_numpy_array_data
+
+
+
+
 class ModelTrainer:
     def __init__(self):
         pass
